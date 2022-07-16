b7b5240c6b71d96f441aa2362a10afd47538d907
Sayyad Ibrahimov <69716805+ibrahimov13@users.noreply.github.com>
2022-07-16 12:52:43 +0400

    Update Javascript file

--- js/script.js
+++ js/script.js
@@ -1,127 +1,76 @@
-const fileInput = document.querySelector(".file-input"),
-filterOptions = document.querySelectorAll(".filter button"),
-filterName = document.querySelector(".filter-info .name"),
-filterValue = document.querySelector(".filter-info .value"),
-filterSlider = document.querySelector(".slider input"),
-rotateOptions = document.querySelectorAll(".rotate button"),
-previewImg = document.querySelector(".preview-img img"),
-resetFilterBtn = document.querySelector(".reset-filter"),
-chooseImgBtn = document.querySelector(".choose-img"),
-saveImgBtn = document.querySelector(".save-img");
+const fromText = document.querySelector(".from-text"),
+toText = document.querySelector(".to-text"),
+selectTag = document.querySelectorAll("select"),
+exchangeIcon = document.querySelector(".exchange"),
+translateBtn = document.querySelector("button"),
+icons = document.querySelectorAll(".row i");
 
-let brightness = 100, saturation = 100, inversion = 0, grayscale = 0;
-let rotate = 0, flipHorizontal = 1, flipVertical = 1;
-
-const applyFilters = () => {
-    previewImg.style.transform = `rotate(${rotate}deg) scale(${flipHorizontal}, ${flipVertical})`;
-    previewImg.style.filter = `brightness(${brightness}%) saturate(${saturation}%) invert(${inversion}%) grayscale(${grayscale}%)`;
-}
-
-const loadImage = () => {
-    let file = fileInput.files[0]; // getting user selected file
-    if(!file) return; // return if user hasn't selected file
-    console.log(file);
-    previewImg.src = URL.createObjectURL(file); // passing file url as preview img src
-    previewImg.addEventListener("load", () => {
-        resetFilterBtn.click(); // clicking reset btn, so the filter value reset if the user select new img
-        document.querySelector(".container").classList.remove("disable");
-    });
-}
-
-filterOptions.forEach(option => {
-    option.addEventListener("click", () => { // adding click event listener to all filter buttons
-        document.querySelector(".filter .active").classList.remove("active");
-        option.classList.add("active");
-        filterName.innerText = option.innerText;
-
-        if(option.id === "brightness"){
-            filterSlider.max = "200";
-            filterSlider.value = brightness;
-            filterValue.innerText = `${brightness}%`;
-        }else if(option.id === "saturation"){
-            filterSlider.max = "200";
-            filterSlider.value = saturation;
-            filterValue.innerText = `${saturation}%`;
-        }else if(option.id === "inversion"){
-            filterSlider.max = "100";
-            filterSlider.value = inversion;
-            filterValue.innerText = `${inversion}%`;
-        }else{
-            filterSlider.max = "100";
-            filterSlider.value = grayscale;
-            filterValue.innerText = `${grayscale}%`;
+selectTag.forEach((tag, id) => {
+    console.log(tag);
+    for (const country_code in countries) {
+        console.log(countries[country_code]);
+        // selecting English by default as FROM language and Hindi as TO language
+        let selected;
+        if(id == 0 && country_code == "en-GB"){
+            selected = "selected";
+        }else if(id == 1 && country_code == "hi-IN"){
+            selected = "selected";
         }
+        let option = `<option value="${country_code}" ${selected}>${countries[country_code]}</option>`;
+        tag.insertAdjacentHTML("beforeend", option); // adding options tag inside select tag
+    }
+});
+
+exchangeIcon.addEventListener("click", () => {
+    // exchanging textarea and select tag values
+    let tempText = fromText.value;
+    tempLang = selectTag[0].value;
+    selectTag[0].value = selectTag[1].value;
+    fromText.value = toText.value;
+    toText.value = tempText;
+    selectTag[1].value = tempLang;
+});
+
+translateBtn.addEventListener("click", () => {
+    let text = fromText.value,
+    translateFrom = selectTag[0].value, // getting fromSelect tag value
+    translateTo = selectTag[1].value; // getting toSelect tag value
+    if(!text) return;
+    toText.setAttribute("placeholder", "Translating...");
+    let apiUrl = `https://api.mymemory.translated.net/get?q=${text}!&langpair=${translateFrom}|${translateTo}`;
+    // fetching api response and returning it with parsing into js obj
+    // and in another then method receiving that obj
+    fetch(apiUrl).then(res => res.json()).then(data => {
+        console.log(data);
+        toText.value = data.responseData.translatedText;
+        toText.setAttribute("placeholder", "Translation");
     });
 });
 
-const updateFilter = () => {
-    filterValue.innerText = `${filterSlider.value}%`;
-    const selectedFilter = document.querySelector(".filter .active"); // getting selected filter btn
-
-    if(selectedFilter.id === "brightness"){
-        brightness = filterSlider.value;
-    }else if(selectedFilter.id === "saturation"){
-        saturation = filterSlider.value;
-    }else if(selectedFilter.id === "inversion"){
-        inversion = filterSlider.value;
-    }else{
-        grayscale = filterSlider.value;
-    }
-    applyFilters();
-}
-
-rotateOptions.forEach(option => {
-    option.addEventListener("click", () => { // adding click event listener to all rotate/flip buttons
-        console.log(option);
-        if(option.id === "left"){
-            rotate -= 90; // if clicked btn is left rotate, decrement rotate value by -90
-        }else if(option.id === "right"){
-            rotate += 90; // if clicked btn is right rotate, increment rotate value by +90
-        }else if(option.id === "horizontal"){
-            // if flipHorizontal value is 1, set this value to -1 else set 1
-            flipHorizontal = flipHorizontal === 1 ? -1 : 1;
+icons.forEach(icon => {
+    icon.addEventListener("click", ({target}) => {
+        console.log(target);
+        if(target.classList.contains("fa-copy")){
+            // if clicked icon has from id, copy the fromTextarea value else copy the toTextarea value
+            if(target.id == "from"){
+                console.log("From copy icon clicked");
+                navigator.clipboard.writeText(fromText.value);
+            }else{
+                console.log("To copy icon clicked");
+                navigator.clipboard.writeText(toText.value);
+            }
         }else{
-            // if flipVertical value is 1, set this value to -1 else set 1
-            flipVertical = flipVertical === 1 ? -1 : 1;
+            console.log("Speech icon clicked");
+            let utterance;
+            // if clicked icon has from id, speak the fromTextarea value else speak the toTextarea value
+            if(target.id == "from"){
+                utterance = new SpeechSynthesisUtterance(fromText.value);
+                utterance.lang = selectTag[0].value; // setting utterance language to fromSelect tag value
+            }else{
+                utterance = new SpeechSynthesisUtterance(toText.value);
+                utterance.lang = selectTag[1].value; // setting utterance language to toSelect tag value
+            }
+            speechSynthesis.speak(utterance); // speak the passed utterance
         }
-        applyFilters();
     });
 });
-
-const resetFilter = () => {
-    // resetting all variable value to its default value
-    brightness = 100, saturation = 100, inversion = 0, grayscale = 0;
-    rotate = 0, flipHorizontal = 1, flipVertical = 1;
-    filterOptions[0].click(); // clicking brightness btn, so the brightness selected by default
-    applyFilters();
-}
-
-const saveImage = () => {
-    console.log("Save image btn clciked");
-    const canvas = document.createElement("canvas"); // creating canvas element
-    const ctx = canvas.getContext("2d"); // canvas.getContext return a drawing context on the canvas
-    canvas.width = previewImg.naturalWidth; // setting canvas width to actual image width
-    canvas.height = previewImg.naturalHeight; // setting canvas height to actual image height
-
-    // applying user selected filters to canvas filter
-    ctx.filter = `brightness(${brightness}%) saturate(${saturation}%) invert(${inversion}%) grayscale(${grayscale}%)`;
-    ctx.translate(canvas.width / 2, canvas.height / 2); // translating canvas from center
-    if(rotate !== 0){ // if rotate value isn't 0, rotate the canvas
-        ctx.rotate(rotate * Math.PI / 180);
-    }
-    ctx.scale(flipHorizontal, flipVertical); // flip canvas, horizontally, vertically
-    ctx.drawImage(previewImg, -canvas.width / 2, -canvas.height / 2, canvas.width, canvas.height);
-    // document.body.appendChild(canvas);
-
-    const link = document.createElement("a"); // creating <a> element
-    link.download = "image.jpg"; // passing <a> tag download value to "image.jpg"
-    link.href = canvas.toDataURL(); // passing <a> tag href value to canvas data url
-    link.click(); // clicking <a> tag so the image download
-}
-
-
-fileInput.addEventListener("change", loadImage);
-filterSlider.addEventListener("input", updateFilter);
-resetFilterBtn.addEventListener("click", resetFilter);
-saveImgBtn.addEventListener("click", saveImage);
-chooseImgBtn.addEventListener("click", () => fileInput.click());
